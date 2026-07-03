eb45130d27aa02cbce01116a46a9468583b144ab
Md Tanzib Hossain <md.tanzib.hossain@gmail.com>
2026-07-03 11:31:31 +0600

    feat(loan): add StaffLoanResource and LoanScheduleResource

---
+++ app/Modules/Loan/Http/Resources/StaffLoanResource.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Modules\Loan\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+/** @mixin \App\Modules\Loan\Models\StaffLoan */
+class StaffLoanResource extends JsonResource
+{
+    public function toArray(Request $request): array
+    {
+        return [
+            'id'       => $this->id,
+            'staff_id' => $this->staff_id,
+            'staff'    => $this->whenLoaded('staff', fn () => [
+                'id'          => $this->staff->id,
+                'name'        => $this->staff->name,
+                'employee_id' => $this->staff->employee_id,
+            ]),
+            'requested_amount'  => $this->requested_amount,
+            'installment_count' => $this->installment_count,
+            'reason'            => $this->reason,
+            'start_date'        => $this->start_date->toDateString(),
+            'status'            => $this->status,
+            'requested_by'      => $this->requested_by,
+            'approved_by'       => $this->approved_by,
+            'approved_at'       => $this->approved_at?->toIso8601String(),
+            'rejection_reason'  => $this->rejection_reason,
+            'schedules'         => LoanScheduleResource::collection($this->whenLoaded('schedules')),
+        ];
+    }
+}
